0e8dda320d8f118237438ec135bfabb24c595bf7
Jonathon Hellmann <jonatho7@vt.edu>
2014-07-21 17:29:58 -0400

    Added class DateUtil and DateUtilUsage

---
+++ Utilities/src/main/java/hellmann/utility/DateUtilUsage.java
@@ -0,0 +1,36 @@
+package hellmann.utility;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Date;
+
+public class DateUtilUsage {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+        //Create the JSON object.
+        String content = null;
+        try {
+            content = readFile("resources/asia_mov_changed2.json", StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        JSONObject json = new JSONObject(content);
+
+        Date date = DateUtil.extractDateFromJSON(json);
+        System.out.println(date.toGMTString());
+    }
+
+    /**
+     * Creates a String from the contents of a file.
+     */
+    public static String readFile(String path, Charset encoding) throws IOException {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded, encoding);
+    }
+}
