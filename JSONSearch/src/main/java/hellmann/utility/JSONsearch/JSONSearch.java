922ead33c6d5043b9bc76cc44f13b5a172830991
Jonathon Hellmann <jonatho7@vt.edu>
2014-08-06 15:26:19 -0400

    Added more methods to the JSONSearch class.

--- JSONSearch/src/main/java/hellmann/utility/JSONsearch/JSONSearch.java
+++ JSONSearch/src/main/java/hellmann/utility/JSONsearch/JSONSearch.java
@@ -7,14 +7,45 @@ import java.util.ArrayList;
 
 public class JSONSearch {
 
+    public static Object searchForObject(JSONObject json, String name) {
+        Object value = recursiveSearch(null, json, name, null, null);
+        return value;
+    }
+
+    public static ArrayList<Object> searchForObjects(JSONObject json, String name) {
+        ArrayList<Object> myList = new ArrayList<Object>();
+        recursiveSearch(null, json, name, null, myList);
+        return myList;
+    }
+
     public static Boolean searchForBoolean(JSONObject json, String name) {
         Boolean value = (Boolean) recursiveSearch(null, json, name, Boolean.class, null);
         return value;
     }
 
+    public static ArrayList<Boolean> searchForBooleans(JSONObject json, String name) {
+        ArrayList<Object> myList = new ArrayList<Object>();
+        recursiveSearch(null, json, name, Boolean.class, myList);
+        ArrayList<Boolean> newList = new ArrayList<Boolean>();
+        for (int i = 0; i < myList.size(); i++) {
+            newList.add((Boolean) myList.get(i));
+        }
+        return newList;
+    }
+
     public static String searchForString(JSONObject json, String name) {
         String value = (String) recursiveSearch(null, json, name, String.class, null);
         return value;
+    }
+
+    public static ArrayList<String> searchForStrings(JSONObject json, String name) {
+        ArrayList<Object> myList = new ArrayList<Object>();
+        recursiveSearch(null, json, name, String.class, myList);
+        ArrayList<String> newList = new ArrayList<String>();
+        for (int i = 0; i < myList.size(); i++) {
+            newList.add((String) myList.get(i));
+        }
+        return newList;
     }
 
     /**
@@ -28,18 +59,32 @@ public class JSONSearch {
      */
     public static Double searchForDouble(JSONObject json, String name) {
         Object value = recursiveSearch(null, json, name, Double.class, null);
+        return convertToDouble(value);
+    }
+
+    public static ArrayList<Double> searchForDoubles(JSONObject json, String name) {
+        ArrayList<Object> myList = new ArrayList<Object>();
+        recursiveSearch(null, json, name, Double.class, myList);
+        ArrayList<Double> newList = new ArrayList<Double>();
+        for (int i = 0; i < myList.size(); i++) {
+            newList.add(convertToDouble(myList.get(i)));
+        }
+        return newList;
+    }
+
+    private static Double convertToDouble(Object value) {
         if (value != null) {
             if (value.getClass() == Double.class) {
-                return new Double((Double) value);
+                return (Double) value;
             } else if (value.getClass() == Long.class) {
                 return new Double(((Long) value).doubleValue());
             } else if (value.getClass() == Integer.class) {
                 return new Double(((Integer) value).doubleValue());
             }
         }
-
         return null;
     }
+
 
     /**
      * Search for a long value. Will return results for a long or int value.
@@ -61,8 +106,30 @@ public class JSONSearch {
         return null;
     }
 
+    public static ArrayList<Long> searchForLongs(JSONObject json, String name) {
+        ArrayList<Object> myList = new ArrayList<Object>();
+        recursiveSearch(null, json, name, Long.class, myList);
+        ArrayList<Long> newList = new ArrayList<Long>();
+        for (int i = 0; i < myList.size(); i++) {
+            newList.add(convertToLong(myList.get(i)));
+        }
+        return newList;
+    }
+
+    private static Long convertToLong(Object value) {
+        if (value != null) {
+            if (value.getClass() == Long.class) {
+                return (Long) value;
+            } else if (value.getClass() == Integer.class) {
+                return new Long(((Integer) value).longValue());
+            }
+        }
+        return null;
+    }
+
+
     /**
-     * Search for an int value.
+     * Search for an int value that has a particular name.
      *
      * @param json
      * @param name
@@ -73,30 +140,93 @@ public class JSONSearch {
         return value;
     }
 
+    public static ArrayList<Integer> searchForIntegers(JSONObject json, String name) {
+        ArrayList<Object> myList = new ArrayList<Object>();
+        recursiveSearch(null, json, name, Integer.class, myList);
+        ArrayList<Integer> newList = new ArrayList<Integer>();
+        for (int i = 0; i < myList.size(); i++) {
+            newList.add((Integer) myList.get(i));
+        }
+        return newList;
+    }
+
+
     public static JSONObject searchForJSONObject(JSONObject json, String name) {
         JSONObject value = (JSONObject) recursiveSearch(null, json, name, JSONObject.class, null);
         return value;
     }
 
+    public static ArrayList<JSONObject> searchForJSONObjects(JSONObject json, String name) {
+        ArrayList<Object> myList = new ArrayList<Object>();
+        recursiveSearch(null, json, name, JSONObject.class, myList);
+        ArrayList<JSONObject> newList = new ArrayList<JSONObject>();
+        for (int i = 0; i < myList.size(); i++) {
+            newList.add((JSONObject) myList.get(i));
+        }
+        return newList;
+    }
+
+
+    /**
+     * Search for a JSONArray that has a particular name.
+     *
+     * @param json
+     * @param name
+     * @return
+     */
     public static JSONArray searchForJSONArray(JSONObject json, String name) {
         JSONArray value = (JSONArray) recursiveSearch(null, json, name, JSONArray.class, null);
         return value;
     }
 
-    public static JSONArray searchForJSONArray(JSONObject json, Object[] values){
-
-        return null;
+    /**
+     * Searches for all JSONArrays that have a particular name.
+     *
+     * @param json
+     * @return
+     */
+    public static ArrayList<JSONArray> searchForJSONArrays(JSONObject json, String name) {
+        ArrayList<Object> myList = new ArrayList<Object>();
+        recursiveSearch(null, json, name, JSONArray.class, myList);
+        ArrayList<JSONArray> newList = new ArrayList<JSONArray>();
+        for (int i = 0; i < myList.size(); i++) {
+            newList.add((JSONArray) myList.get(i));
+        }
+        return newList;
     }
 
-    public static ArrayList<Object> searchForJSONArrays(JSONObject json){
+    /**
+     * Searches for all JSONArrays that do not have a name.
+     *
+     * @param json
+     * @return
+     */
+    public static ArrayList<JSONArray> searchForJSONArrays(JSONObject json) {
         ArrayList<Object> myList = new ArrayList<Object>();
         recursiveSearch(null, json, null, JSONArray.class, myList);
+        ArrayList<JSONArray> newList = new ArrayList<JSONArray>();
+        for (int i = 0; i < myList.size(); i++) {
+            newList.add((JSONArray) myList.get(i));
+        }
+        return newList;
+    }
+
+    /**
+     * Search for all JSONArrays that have a particular set of values.
+     *
+     * @param json
+     * @param values
+     * @return
+     */
+    //TODO. This method has not been developed yet.
+    public static JSONArray searchForJSONArray(JSONObject json, Object[] values) {
+
         return null;
     }
 
 
-    //TODO. Work on the myList functionality, in order to search for multiple instances of an object.
-    // So far have only started this part of the code. Did not finish this yet.
+    //if myList is null, this means we are not trying to add items to a list.
+    //if myList is not null, we shall add the items to myList.
     private static Object recursiveSearch(String objectToReduceName,
                                           Object objectToReduceValue,
                                           String requestedName,
@@ -107,14 +237,26 @@ public class JSONSearch {
             return null;
         } else if (objectToReduceValue.getClass() == JSONArray.class) {
             //Then we have a JSONArray. Is it one without a name?
-            if (requestedName == null){
+            if (requestedName == null
+                    && objectToReduceName == null) {
                 JSONArray jsonArray = (JSONArray) objectToReduceValue;
-                myList.add(jsonArray);
+                if (myList == null) {
+                    return jsonArray;
+                } else {
+                    myList.add(jsonArray);
+                }
             }
             //Then we have a JSONArray with a name. Is this what we wanted to return?
-            if (requestedName.equals(objectToReduceName) && objectToReduceValue.getClass() == requestedObjectType) {
-                //Then we have a JSONArray with the requested name and return type. return this.
-                return objectToReduceValue;
+            if (requestedName != null
+                    && requestedName.equals(objectToReduceName)
+                    && (objectToReduceValue.getClass() == requestedObjectType
+                    || requestedObjectType == null)) {
+                //Then we have a JSONArray with the requested name and return type. Shall we return this or just add.
+                if (myList == null) {
+                    return objectToReduceValue;
+                } else {
+                    myList.add(objectToReduceValue);
+                }
             }
             //Otherwise, Break down the JSONArray.
             JSONArray jsonArray = (JSONArray) objectToReduceValue;
@@ -127,9 +269,16 @@ public class JSONSearch {
             }
         } else if (objectToReduceValue.getClass() == JSONObject.class) {
             //Then we have a JSONObject. Is this what we wanted to return?
-            if (requestedName.equals(objectToReduceName) && objectToReduceValue.getClass() == requestedObjectType) {
+            if (requestedName != null
+                    && requestedName.equals(objectToReduceName)
+                    && (objectToReduceValue.getClass() == requestedObjectType
+                        || requestedObjectType == null)) {
                 //Then we have a JSONObject with the requested name and return type. return this.
-                return objectToReduceValue;
+                if (myList == null) {
+                    return objectToReduceValue;
+                } else {
+                    myList.add(objectToReduceValue);
+                }
             }
             //Otherwise, Break down the JSONObject.
             JSONObject jsonObject = (JSONObject) objectToReduceValue;
@@ -145,39 +294,66 @@ public class JSONSearch {
                 }
             }
         } else if (objectToReduceValue.getClass() == Double.class) {
-            if (requestedObjectType == Double.class) {
-                if (requestedName.equals(objectToReduceName)) {
+            if (requestedObjectType == Double.class || requestedObjectType == null) {
+                if (requestedName != null
+                        && requestedName.equals(objectToReduceName)) {
                     //Check if objectToReduceValue is a valid double.
                     if (!Double.isNaN((Double) objectToReduceValue)) {
-                        return objectToReduceValue;
+                        if (myList == null) {
+                            return objectToReduceValue;
+                        } else {
+                            myList.add(objectToReduceValue);
+                        }
                     }
                 }
             }
         } else if (objectToReduceValue.getClass() == Long.class) {
             if (requestedObjectType == Long.class ||
-                    requestedObjectType == Integer.class) {
-                if (requestedName.equals(objectToReduceName)) {
-                    return objectToReduceValue;
+                    requestedObjectType == Integer.class ||
+                    requestedObjectType == null) {
+                if (requestedName != null
+                        && requestedName.equals(objectToReduceName)) {
+                    if (myList == null) {
+                        return objectToReduceValue;
+                    } else {
+                        myList.add(objectToReduceValue);
+                    }
                 }
             }
         } else if (objectToReduceValue.getClass() == Integer.class) {
             if (requestedObjectType == Integer.class ||
                     requestedObjectType == Double.class ||
-                    requestedObjectType == Long.class) {
-                if (requestedName.equals(objectToReduceName)) {
-                    return objectToReduceValue;
+                    requestedObjectType == Long.class ||
+                    requestedObjectType == null) {
+                if (requestedName != null
+                        && requestedName.equals(objectToReduceName)) {
+                    if (myList == null) {
+                        return objectToReduceValue;
+                    } else {
+                        myList.add(objectToReduceValue);
+                    }
                 }
             }
         } else if (objectToReduceValue.getClass() == String.class) {
-            if (requestedObjectType == String.class) {
-                if (requestedName.equals(objectToReduceName)) {
-                    return objectToReduceValue;
+            if (requestedObjectType == String.class || requestedObjectType == null) {
+                if (requestedName != null
+                        && requestedName.equals(objectToReduceName)) {
+                    if (myList == null) {
+                        return objectToReduceValue;
+                    } else {
+                        myList.add(objectToReduceValue);
+                    }
                 }
             }
         } else if (objectToReduceValue.getClass() == Boolean.class) {
-            if (requestedObjectType == Boolean.class) {
-                if (requestedName.equals(objectToReduceName)) {
-                    return objectToReduceValue;
+            if (requestedObjectType == Boolean.class || requestedObjectType == null) {
+                if (requestedName != null
+                        && requestedName.equals(objectToReduceName)) {
+                    if (myList == null) {
+                        return objectToReduceValue;
+                    } else {
+                        myList.add(objectToReduceValue);
+                    }
                 }
             }
         }
--- JSONSearch/src/main/java/hellmann/utility/JSONsearch/JSONSearchUsage.java
+++ JSONSearch/src/main/java/hellmann/utility/JSONsearch/JSONSearchUsage.java
@@ -61,11 +61,67 @@ public class JSONSearchUsage {
         JSONArray jsonArray = JSONSearch.searchForJSONArray(json, "streams");
         System.out.println("JSONArray = " + jsonArray);
 
-        Boolean bool = JSONSearch.searchForBoolean(json, "simple");
+        Boolean bool = JSONSearch.searchForBoolean(json, "answer");
         System.out.println("Boolean = " + bool);
 
-        ArrayList<Object> myList = JSONSearch.searchForJSONArrays(json);
-        System.out.println(myList.toArray());
+        ArrayList<JSONArray> myList = JSONSearch.searchForJSONArrays(json);
+        System.out.println(myList.size() + " JSONArrays without a name");
+        for(int i = 0; i < myList.size(); i++){
+            System.out.println( ((JSONArray) myList.get(i)).toString(4));
+        }
+
+        ArrayList<JSONArray> myListing = JSONSearch.searchForJSONArrays(json, "cool");
+        System.out.println(myListing.size() + " JSONArrays with the name cool");
+        for(int i = 0; i < myListing.size(); i++){
+            System.out.println( ((JSONArray) myListing.get(i)).toString(4));
+        }
+
+        ArrayList<JSONObject> jsonObjects = JSONSearch.searchForJSONObjects(json, "objective");
+        System.out.println(jsonObjects.size() + " JSONObjects with the name objective");
+        for(int i = 0; i < jsonObjects.size(); i++){
+            System.out.println( ((JSONObject) jsonObjects.get(i)).toString(4));
+        }
+
+        ArrayList<Integer> integerList = JSONSearch.searchForIntegers(json, "rotate");
+        System.out.println(integerList.size() + " Integers with the name rotate");
+        for(int i = 0; i < integerList.size(); i++){
+            System.out.println( integerList.get(i).toString());
+        }
+
+        ArrayList<Long> longsList = JSONSearch.searchForLongs(json, "rotate");
+        System.out.println(longsList.size() + " Longs with the name rotate");
+        for(int i = 0; i < longsList.size(); i++){
+            System.out.println(  longsList.get(i).toString());
+        }
+
+        ArrayList<Double> doublesList = JSONSearch.searchForDoubles(json, "rotate");
+        System.out.println(doublesList.size() + " Doubles with the name rotate");
+        for(int i = 0; i < doublesList.size(); i++){
+            System.out.println( doublesList.get(i).toString());
+        }
+
+        ArrayList<String> stringsList = JSONSearch.searchForStrings(json, "profile");
+        System.out.println(stringsList.size() + " Strings with the name profile");
+        for(int i = 0; i < stringsList.size(); i++){
+            System.out.println( stringsList.get(i).toString());
+        }
+
+        ArrayList<Boolean> booleansList = JSONSearch.searchForBooleans(json, "answer");
+        System.out.println(booleansList.size() + " Booleans with the name answer");
+        for(int i = 0; i < booleansList.size(); i++){
+            System.out.println( booleansList.get(i).toString());
+        }
+
+        ArrayList<Object> objectsList = JSONSearch.searchForObjects(json, "rotate");
+        System.out.println(objectsList.size() + " Objects with the name rotate");
+        for(int i = 0; i < objectsList.size(); i++){
+            System.out.println( objectsList.get(i).toString());
+        }
+
+        Object myObject = JSONSearch.searchForObject(json, "rotate");
+        System.out.println("The first Object with the name rotate found = " + myObject);
+
+
 
     }
 
